31c129a9b24b02b18d06f05f9a64097b9fc14f55
kalinin <prozaik81-2@yandex.ru>
2020-12-31 16:10:31 +0300

    inner layout ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { BrowserRouter, Route, NavLink, Switch, Redirect } from "react-router-dom"
-import './App.css'
 
+import Grid from '@material-ui/core/Grid'
+import Icon from '@material-ui/core/Icon';
+import Typography from '@material-ui/core/Typography';
+
+import './App.css'
 import My from './components/pages/My'
 import News from './components/pages/News'
 import Friends from './components/pages/Friends'
@@ -11,26 +15,37 @@ function App() {
   return (
     <BrowserRouter>
       <div className="wrap">
-        <table>
-          <tr>
-            <td className="nav">
+
+        <header className="header">
+          <div className="header-inner">
+            <Icon>stars</Icon>
+            <Typography>VKiller</Typography>            
+          </div>
+        </header>        
+
+        <Grid container spacing={ 1 }>
+          <Grid container item xs={ 2 } spacing={ 3 } className="nav">
+            <Grid item>
               <NavLink to="/" className="nav-link">My</NavLink>
               <NavLink to="/news" className="nav-link">News</NavLink>
               <NavLink to="/friends" className="nav-link">Friends</NavLink>
-            </td>
+            </Grid>
+          </Grid>
 
-            <td className="content">
+          <Grid container item xs={10} spacing={3} className="content">
+            <Grid item>
               <Switch>
                 <Route exact path='/' render={ () => <My/> }/>
                 <Route exact path='/My' render={ () => <My/> }/>
                 <Route exact path='/News' render={ () => <News/> }/>
                 <Route exact path='/Friends' render={ () => <Friends/> }/>
                 <Route exact path='/Page404' render={ () => <Page404/> }/>
-                <Redirect to="/Page404" />
+                <Redirect to="/Page404"/>
               </Switch>
-            </td>
-          </tr>
-        </table>
+            </Grid>
+          </Grid>
+        </Grid>
+
       </div>
     </BrowserRouter>
   )
