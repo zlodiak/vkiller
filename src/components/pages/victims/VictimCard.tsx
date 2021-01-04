0102b4bf9ab294186ee1b04d46137f8a10de7134
kalinin <prozaik81-2@yandex.ru>
2021-01-04 16:32:36 +0300

    capitalize keys

--- src/components/pages/victims/VictimCard.tsx
+++ src/components/pages/victims/VictimCard.tsx
@@ -14,35 +14,36 @@ function VictimCard(props: any) {
 
   function displayFields() {
     const fields: any = prepareFieldsForCard(props.fields)
+    debugger
     return (
       Object.keys(fields)
-        .map((key: string, i: number) => {
-          if(key === 'id_user') { return }
-          return (
-            <div className={ s.line } key={ i }>
-              <MUI.Typography variant="caption" gutterBottom className={ s.label }>
-                { key }
-              </MUI.Typography>
-              <MUI.Typography variant="body1" gutterBottom>
-                { fields[key] }
-              </MUI.Typography>           
-            </div>
-          )
-        })
+      .map((key: string, i: number) => {
+        if(key === 'id_user') { return }
+        return (
+          <div className={ s.line } key={ i }>
+            <MUI.Typography variant="caption" gutterBottom className={ s.label }>
+              { key.replace('_', ' ') }
+            </MUI.Typography>
+            <MUI.Typography variant="body1" gutterBottom>
+              { fields[key] }
+            </MUI.Typography>
+          </div>
+        )
+      })      
     )
   }
 
-  return(
+  return (
     <MUI.Card className={ s.card }>
       <MUI.CardContent>
-        { displayFields() }         
+        { displayFields() }      
       </MUI.CardContent>
 
       <MUI.CardActions>
-        <MUI.Button variant="outlined" size="small">Open details</MUI.Button>
+        <MUI.Button variant="outlined" size="small" onClick={ () => VictimCard(11) }>Open details</MUI.Button>
         <MUI.Button variant="outlined" size="small">Toggle Status</MUI.Button>
       </MUI.CardActions>
-    </MUI.Card>
+    </MUI.Card>      
   )
 }
 
